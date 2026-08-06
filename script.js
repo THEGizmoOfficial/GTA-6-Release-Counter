a8d3e8e06cea9cdad79206439ace8918bb2518c6
THEGizmoOfficial <79152243+THEGizmoOfficial@users.noreply.github.com>
2026-08-06 20:13:58 +0200

    Extended Look GTA VI

--- script.js
+++ script.js
@@ -1,74 +1,107 @@
-let days = document.getElementById('day');
-let hours = document.getElementById('hour');
-let minutes = document.getElementById('minute');
-let seconds = document.getElementById('second');
+const endDate = new Date("2026-11-19T00:00:00").getTime();
 
-let dd = document.getElementById('dd');
-let hh = document.getElementById('hh');
-let mm = document.getElementById('mm');
-let ss = document.getElementById('ss');
+const day = document.getElementById("day");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
 
-let daysDot = document.querySelector('.dayDot');
-let hoursDot = document.querySelector('.hourDot');
-let minutesDot = document.querySelector('.minuteDot');
-let secondsDot = document.querySelector('.secondDot');
+const dd = document.getElementById("dd");
+const hh = document.getElementById("hh");
+const mm = document.getElementById("mm");
+const ss = document.getElementById("ss");
 
-let preorderDate = '2026-06-25T00:00:00';
-let endDate = '2026-11-19T00:00:00';
+const dayDot = document.querySelector(".dayDot");
+const hourDot = document.querySelector(".hourDot");
+const minuteDot = document.querySelector(".minuteDot");
+const secondDot = document.querySelector(".secondDot");
 
-let preorderDays = document.getElementById("preorderDays");
-let preorderHours = document.getElementById("preorderHours");
-let preorderMinutes = document.getElementById("preorderMinutes");
-let preorderSeconds = document.getElementById("preorderSeconds");
+const preorderDate = new Date("2026-06-25T00:00:00").getTime();
 
-let x = setInterval(function () {
-    let now = new Date(endDate).getTime();
-    let countDown = new Date().getTime();
-    let distance = now - countDown;
+const preorderDays = document.getElementById("preorderDays");
+const preorderHours = document.getElementById("preorderHours");
+const preorderMinutes = document.getElementById("preorderMinutes");
+const preorderSeconds = document.getElementById("preorderSeconds");
+
+const trailerDate = new Date("2026-08-27T19:00:00Z").getTime();
+
+const trailerDays = document.getElementById("trailerDays");
+const trailerHours = document.getElementById("trailerHours");
+const trailerMinutes = document.getElementById("trailerMinutes");
+const trailerSeconds = document.getElementById("trailerSeconds");
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    let distance = endDate - now;
 
     let d = Math.floor(distance / (1000 * 60 * 60 * 24));
     let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let s = Math.floor((distance % (1000 * 60)) / (1000));
+    let s = Math.floor((distance % (1000 * 60)) / 1000);
 
-    days.innerHTML = d + "<br><span>Dni</span>";
-    hours.innerHTML = h + "<br><span>Godzin</span>";
-    minutes.innerHTML = m + "<br><span>Minut</span>";
-    seconds.innerHTML = s + "<br><span>Sekund</span>";
+    day.innerHTML = d + "<br><span>Dni</span>";
+    hour.innerHTML = h + "<br><span>Godzin</span>";
+    minute.innerHTML = m + "<br><span>Minut</span>";
+    second.innerHTML = s + "<br><span>Sekund</span>";
 
     dd.style.strokeDashoffset = 440 - (440 * d) / 365;
     hh.style.strokeDashoffset = 440 - (440 * h) / 24;
     mm.style.strokeDashoffset = 440 - (440 * m) / 60;
     ss.style.strokeDashoffset = 440 - (440 * s) / 60;
 
-    daysDot.style.transform = `rotateZ(${d * 0.986}deg)`;
-    hoursDot.style.transform = `rotateZ(${h * 15}deg)`;
-    minutesDot.style.transform = `rotateZ(${m * 6}deg)`;
-    secondsDot.style.transform = `rotateZ(${s * 6}deg)`;
+    dayDot.style.transform = `rotate(${d * 0.986}deg)`;
+    hourDot.style.transform = `rotate(${h * 15}deg)`;
+    minuteDot.style.transform = `rotate(${m * 6}deg)`;
+    secondDot.style.transform = `rotate(${s * 6}deg)`;
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timeCounter").style.display = 'none';
-        document.querySelector(".announcement").style.display = 'block';
-    };
+    if (distance <= 0) {
 
-    let preorderTarget = new Date(preorderDate).getTime();
-    let preorderDistance = preorderTarget - countDown;
+        document.getElementById("timeCounter").style.display = "none";
+        document.querySelector(".announcement").style.display = "block";
 
-    let pd = Math.floor(preorderDistance / (1000 * 60 * 60 * 24));
-    let ph = Math.floor((preorderDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let pm = Math.floor((preorderDistance % (1000 * 60 * 60)) / (1000 * 60));
-    let ps = Math.floor((preorderDistance % (1000 * 60)) / 1000);
+    }
 
-    preorderDays.innerHTML = pd + "<br><span>Dni</span>";
-    preorderHours.innerHTML = ph + "<br><span>Godzin</span>";
-    preorderMinutes.innerHTML = pm + "<br><span>Minut</span>";
-    preorderSeconds.innerHTML = ps + "<br><span>Sekund</span>";
+    let preorderDistance = preorderDate - now;
 
-    if (preorderDistance <= 0) {
+    if (preorderDistance > 0) {
+
+        let pd = Math.floor(preorderDistance / (1000 * 60 * 60 * 24));
+        let ph = Math.floor((preorderDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let pm = Math.floor((preorderDistance % (1000 * 60 * 60)) / (1000 * 60));
+        let ps = Math.floor((preorderDistance % (1000 * 60)) / 1000);
+
+        preorderDays.innerHTML = pd + "<br><span>Dni</span>";
+        preorderHours.innerHTML = ph + "<br><span>Godzin</span>";
+        preorderMinutes.innerHTML = pm + "<br><span>Minut</span>";
+        preorderSeconds.innerHTML = ps + "<br><span>Sekund</span>";
+
+    } else {
 
         document.getElementById("preorderCounter").style.display = "none";
-
         document.getElementById("preorderButtons").style.display = "flex";
+
     }
-});
+
+    let trailerDistance = trailerDate - now;
+
+    if (trailerDistance > 0) {
+
+        let td = Math.floor(trailerDistance / (1000 * 60 * 60 * 24));
+        let th = Math.floor((trailerDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let tm = Math.floor((trailerDistance % (1000 * 60 * 60)) / (1000 * 60));
+        let ts = Math.floor((trailerDistance % (1000 * 60)) / 1000);
+
+        trailerDays.innerHTML = td + "<br><span>Dni</span>";
+        trailerHours.innerHTML = th + "<br><span>Godzin</span>";
+        trailerMinutes.innerHTML = tm + "<br><span>Minut</span>";
+        trailerSeconds.innerHTML = ts + "<br><span>Sekund</span>";
+
+    } else {
+
+        document.getElementById("trailerCounter").style.display = "none";
+        document.getElementById("trailerButton").style.display = "block";
+
+    }
+
+}, 1000);
