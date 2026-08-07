946de8ee442183d7f81fbde7610831d5e8d53c96
THEGizmoOfficial <79152243+THEGizmoOfficial@users.noreply.github.com>
2026-08-07 19:18:02 +0200

    Fix

--- script.js
+++ script.js
@@ -16,14 +16,12 @@ const minuteDot = document.querySelector(".minuteDot");
 const secondDot = document.querySelector(".secondDot");
 
 const preorderDate = new Date("2026-06-25T00:00:00").getTime();
-
 const preorderDays = document.getElementById("preorderDays");
 const preorderHours = document.getElementById("preorderHours");
 const preorderMinutes = document.getElementById("preorderMinutes");
 const preorderSeconds = document.getElementById("preorderSeconds");
 
 const trailerDate = new Date("2026-08-27T19:00:00Z").getTime();
-
 const trailerDays = document.getElementById("trailerDays");
 const trailerHours = document.getElementById("trailerHours");
 const trailerMinutes = document.getElementById("trailerMinutes");
@@ -32,9 +30,7 @@ const trailerSeconds = document.getElementById("trailerSeconds");
 setInterval(() => {
 
     const now = new Date().getTime();
-
     let distance = endDate - now;
-
     let d = Math.floor(distance / (1000 * 60 * 60 * 24));
     let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -56,52 +52,38 @@ setInterval(() => {
     secondDot.style.transform = `rotate(${s * 6}deg)`;
 
     if (distance <= 0) {
-
         document.getElementById("timeCounter").style.display = "none";
         document.querySelector(".announcement").style.display = "block";
-
     }
 
     let preorderDistance = preorderDate - now;
-
     if (preorderDistance > 0) {
-
         let pd = Math.floor(preorderDistance / (1000 * 60 * 60 * 24));
         let ph = Math.floor((preorderDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let pm = Math.floor((preorderDistance % (1000 * 60 * 60)) / (1000 * 60));
         let ps = Math.floor((preorderDistance % (1000 * 60)) / 1000);
-
         preorderDays.innerHTML = pd + "<br><span>Dni</span>";
         preorderHours.innerHTML = ph + "<br><span>Godzin</span>";
         preorderMinutes.innerHTML = pm + "<br><span>Minut</span>";
         preorderSeconds.innerHTML = ps + "<br><span>Sekund</span>";
-
     } else {
-
         document.getElementById("preorderCounter").style.display = "none";
         document.getElementById("preorderButtons").style.display = "flex";
-
     }
 
     let trailerDistance = trailerDate - now;
-
     if (trailerDistance > 0) {
-
         let td = Math.floor(trailerDistance / (1000 * 60 * 60 * 24));
         let th = Math.floor((trailerDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let tm = Math.floor((trailerDistance % (1000 * 60 * 60)) / (1000 * 60));
         let ts = Math.floor((trailerDistance % (1000 * 60)) / 1000);
-
         trailerDays.innerHTML = td + "<br><span>Dni</span>";
         trailerHours.innerHTML = th + "<br><span>Godzin</span>";
         trailerMinutes.innerHTML = tm + "<br><span>Minut</span>";
         trailerSeconds.innerHTML = ts + "<br><span>Sekund</span>";
-
     } else {
-
         document.getElementById("trailerCounter").style.display = "none";
         document.getElementById("trailerButton").style.display = "block";
-
     }
 
 }, 1000);
